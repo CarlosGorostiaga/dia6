3850bda824c94f41aa2dc11ecb021dc9d1a90900
CarlosGorostiaga <carlosgorospo@gmail.com>
2022-10-18 23:12:17 +0200

    primera subida

---
+++ reto1.js
@@ -0,0 +1,8 @@
+let suma = (op1 = 0, op2 = 0) => (`${op1} + ${op2} = ${op1 + op2}`);
+let resta = (op1 = 0, op2 = 0) => (`${op1} - ${op2} = ${op1 - op2}`);
+let multiplicacion = (op1 = 0, op2 = 0) => (`${op1} * ${op2} = ${op1 * op2}`);
+let division = (op1 = 0, op2 = 0) => (`${op1} / ${op2} = ${op1 / op2}`);
+let cuadrado = (op1 = 0,) => (op1 * op1)
+module.exports = { suma, resta, multiplicacion, division, cuadrado }
+
+console.log(cuadrado(8));
