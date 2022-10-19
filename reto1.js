bb2d8eeae16bb9cb9fd4782fc19a35a193a14782
CarlosGorostiaga <carlosgorospo@gmail.com>
2022-10-19 15:31:11 +0200

    sin suma

--- reto1.js
+++ reto1.js
@@ -4,4 +4,4 @@ let division = (op1 = 0, op2 = 0) => (`${op1} / ${op2} = ${op1 / op2}`);
 let cuadrado = (op1 = 0,) => (op1 * op1)
 module.exports = { suma, resta, multiplicacion, division, cuadrado }
 
-console.log(suma(8, 10, 14));
+console.log(resta(12, 10));
