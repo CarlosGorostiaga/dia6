8862ebe6f2d9141b40b31faeedd8aab05240d42b
CarlosGorostiaga <carlosgorospo@gmail.com>
2022-10-19 16:03:49 +0200

    tt

--- reto1.js
+++ reto1.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 let suma = (op1 = 0, op2 = 0,) => (`${op1} + ${op2} + = ${op1 + op2}`);
-=======
-let suma = (op1 = 0, op2 = 0, op3 = 0 ) => (`${op1} + ${op2} + ${op3} = ${op1 + op2 + op3}`);
->>>>>>> rama2
 let resta = (op1 = 0, op2 = 0) => (`${op1} - ${op2} = ${op1 - op2}`);
 let multiplicacion = (op1 = 0, op2 = 0) => (`${op1} * ${op2} = ${op1 * op2}`);
 let division = (op1 = 0, op2 = 0) => (`${op1} / ${op2} = ${op1 / op2}`);
 let cuadrado = (op1 = 0,) => (op1 * op1)
 module.exports = { suma, resta, multiplicacion, division, cuadrado }
 
-<<<<<<< HEAD
-console.log(resta(12, 10));
-=======
-console.log(suma(8,10,14));
->>>>>>> rama2
+console.log(suma(8,10));
