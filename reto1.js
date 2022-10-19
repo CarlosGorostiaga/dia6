76b4c9591413103f500c51cd4ff75c9fb6193a7c
CarlosGorostiaga <carlosgorospo@gmail.com>
2022-10-19 16:44:22 +0200

    subimos 3op

--- reto1.js
+++ reto1.js
@@ -1,8 +1,10 @@
-let suma = (op1 = 0, op2 = 0, op3 = 0 ) => (`${op1} + ${op2} + ${op3} = ${op1 + op2 + op3}`);
+// suma de 3 op
+let suma = (op1 = 0, op2 = 0, op3 = 0) => (`${op1} + ${op2} + ${op3} = ${op1 - op2 + op3}`);
 let resta = (op1 = 0, op2 = 0) => (`${op1} - ${op2} = ${op1 - op2}`);
 let multiplicacion = (op1 = 0, op2 = 0) => (`${op1} * ${op2} = ${op1 * op2}`);
 let division = (op1 = 0, op2 = 0) => (`${op1} / ${op2} = ${op1 / op2}`);
 let cuadrado = (op1 = 0,) => (op1 * op1)
 module.exports = { suma, resta, multiplicacion, division, cuadrado }
 
-console.log(suma(8,10,14));
+console.log(suma(13, 10, 18));
+
