df9c99ed63e1d6c80cb9eae266433e4d7de988ea
CarlosGorostiaga <carlosgorospo@gmail.com>
2022-10-19 16:37:36 +0200

    prueba

--- reto1.js
+++ reto1.js
@@ -1,3 +1,5 @@
+// sin sumaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
 let resta = (op1 = 0, op2 = 0) => (`${op1} - ${op2} = ${op1 - op2}`);
 let multiplicacion = (op1 = 0, op2 = 0) => (`${op1} * ${op2} = ${op1 * op2}`);
 let division = (op1 = 0, op2 = 0) => (`${op1} / ${op2} = ${op1 / op2}`);
@@ -5,3 +7,4 @@ let cuadrado = (op1 = 0,) => (op1 * op1)
 module.exports = { resta, multiplicacion, division, cuadrado }
 
 console.log(resta(13, 10));
+
