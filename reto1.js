e205736678ca1b881479f2f5777162a1732922e9
CarlosGorostiaga <carlosgorospo@gmail.com>
2022-10-19 15:15:17 +0200

    subida rama 1 sin suma

--- reto1.js
+++ reto1.js
@@ -1,8 +1,7 @@
-
 let resta = (op1 = 0, op2 = 0) => (`${op1} - ${op2} = ${op1 - op2}`);
 let multiplicacion = (op1 = 0, op2 = 0) => (`${op1} * ${op2} = ${op1 * op2}`);
 let division = (op1 = 0, op2 = 0) => (`${op1} / ${op2} = ${op1 / op2}`);
 let cuadrado = (op1 = 0,) => (op1 * op1)
 module.exports = { suma, resta, multiplicacion, division, cuadrado }
 
-console.log(cuadrado(8));
+console.log(suma(8, 10, 14));
